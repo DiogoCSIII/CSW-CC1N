1198b09cfba70bd9c7ded5389929cd61c11d9742
DiogoCSIII <diogocarpanedosoares11@gmail.com>
2025-05-21 22:31:17 -0300

    Add files via upload

---
+++ aula-js-3/jaava.js
@@ -0,0 +1,35 @@
+function verificaridade() {
+    var idade = parseInt(document.getElementById("idade").value)
+    var resultado = document.getElementById("resultado")
+if (idade < 0) 
+    {
+        resultado.textContent = "Idade inválida!"
+    }
+else if (idade == 0)
+    {
+        resultado.textContent = "Você é recém nascido"
+    }
+else if (idade > 0 && idade < 10) 
+    {
+        resultado.textContent = "Você é criança!"
+    }
+else if (idade >= 10 && idade < 16) 
+    {
+        resultado.textContent = "Você é adolescente!"
+    }
+else if (idade >= 16  && idade < 25) 
+    {
+        resultado.textContent = "Você é jovem!"
+    }
+else if (idade >= 25 && idade < 60)
+    {
+        resultado.textContent = "Você é adulto!"
+    }
+else if (idade >= 60)
+    {
+        resultado.textContent = "Você é idoso!"
+    }
+    else (
+        resultado.textContent = "nao ntendi"
+    )
+}
